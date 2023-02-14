5c2aff3b62d2fd472ea5dde22179ab30ef0550f4
Rahul Deep <rahul.deep@gmail.com>
2023-02-14 15:30:49 -0600

    intitial stuff

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambada!'),
+        body: JSON.stringify('Hello from Lambadaaaa!'),
     };
     return response;
 };
