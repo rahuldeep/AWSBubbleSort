2674ee545d1b096969fa5df0ec823c0f8a72d072
Rahul Deep <rahul.deep@gmail.com>
2023-02-14 15:45:30 -0600

    added canned lambda router code

--- index.mjs
+++ index.mjs
@@ -1,8 +1,36 @@
-export const handler = async(event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambadaaaa!'),
-    };
-    return response;
-};
+import * as router from 'aws-lambda-router'
+
+export const handler = router.handler({
+    proxyIntegration: {
+        cors: true,
+        routes: [
+            {
+                // request-path-pattern with a path variable:
+                path: '/article/:id',
+                method: 'GET',
+                // we can use the path param 'id' in the action call:
+                action: (request, context) => {
+                    return "You called me with: " + request.paths.id;
+                }
+            },
+             {
+                // request-path-pattern with a path variable:
+                path: '/',
+                method: 'GET',
+                // we can use the path param 'id' in the action call:
+                action: (request, context) => {
+                    return "You called me with: nothing" ;
+                }
+            },
+            {
+                // request-path-pattern with a path variable in Open API style:
+                path: '/section/{id}',
+                method: 'GET',
+                // we can use the path param 'id' in the action call:
+                action: (request, context) => {
+                    return "You called me with: " + request.paths.id;
+                }
+            }
+        ]
+    }
+})
