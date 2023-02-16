1a3327b3c5fb0cb53345eecb4012935ca3e82fd6
Rahul Deep <rahul.deep@gmail.com>
2023-02-16 13:53:31 -0600

    trying a basic function

--- index.mjs
+++ index.mjs
@@ -48,6 +48,8 @@ export const handler = router.handler({
 
 
 export const handler =  async function(event, context) {
+    
+    //get the query parameters and split them into inutArray
     const inputString = event.queryStringParameters.slist;
     const inputArray = inputString.split(",");
     
@@ -57,26 +59,29 @@ export const handler =  async function(event, context) {
 
     while (!isSorted) {
         isSorted = true;
+        //parse through the array swapping elements
         inputArray.forEach((currentValue, index) => {
             if (currentValue > inputArray[index + 1]) {
                 swapElements(index);
                 swapCount++;
+                //since an element was swapped, it is still not sorted
                 isSorted = false;
             }
         });
     }
+    //send response (array)
     const response = {
         statusCode: 200,
-        body: "The product of is " + inputArray,
+        body: JSON.stringify("The product of is " + inputArray),
     };
     return response;
     
 
-
+//swap element function
 function swapElements(index) {
     [inputArray[index], inputArray[index + 1]] = [inputArray[index + 1], inputArray[index]];
 }
-}
+};
 
 
 
